3fedae6b689eb571ef4013a355604808d1d6125c
MonadWizard <monad.wizard.r@gmail.com>
2023-05-17 12:34:49 +0600

    17_may_2023

--- live_scoring_app/routing.py
+++ live_scoring_app/routing.py
@@ -1,25 +1,13 @@
 # routing.py
 
 from django.urls import path
-<<<<<<< HEAD
-=======
 from live_scoring_app.jwt_auth import JWTAuthMiddleware
->>>>>>> github/production2
 from . import consumers
 
 websocket_urlpatterns = [
     path('live_score/<str:userid>/<str:matchid>/', consumers.SportsScoreConsumer.as_asgi()),
-<<<<<<< HEAD
-    path('live_score_auth/<str:userid>/<str:matchid>/', consumers.SportsScoreConsumerAuth.as_asgi()),
-
-
-
-=======
     path('live_score_auth/<str:matchid>/', JWTAuthMiddleware(consumers.SportsScoreConsumerAuth.as_asgi())),
 
-    # path('live_score_chat/', consumers.ChatConsumer.as_asgi()),
-
     path('live_score_jwt_auth/', JWTAuthMiddleware(consumers.YourConsumer.as_asgi())),
->>>>>>> github/production2
 
 ]
