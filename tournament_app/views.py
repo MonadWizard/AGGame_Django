d6fdb88dab834ea105b2bf7f79e1ea210aa05adc
MonadWizard <monad.wizard.r@gmail.com>
2023-06-05 00:33:29 +0600

    4_june_2023

--- tournament_app/views.py
+++ tournament_app/views.py
@@ -181,6 +181,51 @@ def tournament_schedule(request):
 
 
 
+
+@api_view(['GET','POST'])
+def get_tournament_shedule(request, tournament_id):
+    if request.method =='GET':
+        tournament_id = tournament_id
+        query = f"select get_tournament_shedule_data('{tournament_id}');"
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(query)
+                try:
+                    row = cursor.fetchone()
+                    row = json.loads(row[0])
+                except:
+                    row = cursor.fetchall()
+                return Response(
+                    {
+                        "status": "success",
+                        "data": row,
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            except Exception as e:
+                err_msg = str(e)
+                return Response(
+                    {
+                        "status": "fail",
+                        "message": err_msg,
+                    },
+                    status=status.HTTP_406_NOT_ACCEPTABLE,
+                )
+
+    elif request.method == 'POST':
+        return Response('post data')
+        
+
+
+
+
+
+
+
+
+
+
+
 @api_view(['GET','POST'])
 def upcomming_tournament_list(request):
     if request.method =='GET':
