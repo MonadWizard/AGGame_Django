8c6912f2b956e3b4ad4ad4e49235a5d4ece16815
MonadWizard <monad.wizard.r@gmail.com>
2023-06-14 20:57:35 +0600

    11_june_2023

---
+++ live_scoring_app/views copy.py
@@ -0,0 +1,233 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from django.db import connection
+import json
+
+
+
+
+
+
+@api_view(['GET','POST'])
+def start_match_tournament(request):
+    if request.method =='GET':
+        return Response('get data')
+
+    elif request.method == 'POST':
+        match_id = request.data['match_id']
+        tournament_id = request.data['tournament_id']
+        # print("data::::::::::::",data)
+        query = f"select create_dynamic_table_for_tournament_single_match_start('{match_id}','{tournament_id}');"
+        # print("query::::::::::::",query)
+
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(query)
+                row = cursor.fetchall()
+                return Response(
+                    {
+                        "status": "success",
+                        "data": row,
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            except Exception as e:
+                err_msg = str(e)
+                return Response(
+                    {
+                        "status": "fail",
+                        "message": err_msg,
+                    },
+                    status=status.HTTP_406_NOT_ACCEPTABLE,
+                )
+            
+
+
+
+@api_view(['GET','POST'])
+def input_live_Score_tournament(request):
+    if request.method =='GET':
+        return Response('get data')
+
+    elif request.method == 'POST':
+        data = json.dumps(request.data)
+        # print("data::::::::::::",data)
+        query = f"select input_live_Score_tournament('{data}');"
+        # print("query::::::::::::",query)
+
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(query)
+                # row = cursor.fetchall()
+                # row = cursor.fetchone()
+                # row = json.loads(row[0])
+                return Response(
+                    {
+                        "status": "success",
+                        "data": "input completely complete",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            except Exception as e:
+                err_msg = str(e)
+                return Response(
+                    {
+                        "status": "fail",
+                        "message": err_msg,
+                    },
+                    status=status.HTTP_406_NOT_ACCEPTABLE,
+                )
+            
+
+
+
+
+
+@api_view(['GET','POST'])
+def start_match_game(request):
+    if request.method =='GET':
+        return Response('get data')
+
+    elif request.method == 'POST':
+        data = json.dumps(request.data)
+        print("data::::::::::::",data)
+        query = f"select create_dynamic_table_for_game_match_start('{data}'::jsonb);"
+        print("query::::::::::::",query)
+
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(query)
+                # row = cursor.fetchall()
+                return Response(
+                    {
+                        "status": "success",
+                        "data": "Game match is ready for live score",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            except Exception as e:
+                err_msg = str(e)
+                return Response(
+                    {
+                        "status": "fail",
+                        "message": err_msg,
+                    },
+                    status=status.HTTP_406_NOT_ACCEPTABLE,
+                )
+            
+
+
+@api_view(['GET','POST'])
+def input_live_score_game(request):
+    if request.method =='GET':
+        return Response('get data')
+
+    elif request.method == 'POST':
+        data = json.dumps(request.data)
+        # print("data::::::::::::",data)
+        query = f"select input_live_score_game_update('{data}');"
+        # print("query::::::::::::",query)
+
+        with connection.cursor() as cursor:
+            try:
+                cursor.execute(query)
+                # row = cursor.fetchall()
+                # row = cursor.fetchone()
+                # row = json.loads(row[0])
+                return Response(
+                    {
+                        "status": "success",
+                        "data": "input completely complete",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+            except Exception as e:
+                err_msg = str(e)
+                return Response(
+                    {
+                        "status": "fail",
+                        "message": err_msg,
+                    },
+                    status=status.HTTP_406_NOT_ACCEPTABLE,
+                )
+            
+
+
+
+
+
+
+@api_view(['GET','POST'])
+def ball_type(request):
+    if request.method =='GET':
+        def execute_query(query):
+            with connection.cursor() as cursor:
+                try:
+                    cursor.execute(query)
+                    try:
+                        row = cursor.fetchone()
+                        row = json.loads(row[0])
+                    except:
+                        row = cursor.fetchall()
+                    return {
+                        "status": "success",
+                        "data": row,
+                    }
+                except Exception as e:
+                    err_msg = str(e)
+                    return {
+                        "status": "fail",
+                        "message": err_msg,
+                    }
+
+        @api_view(['GET','POST'])
+        def live_scoring_ball_type(request):
+            if request.method == 'GET':
+                return Response('get data')
+
+            elif request.method == 'POST':
+                data = json.dumps(request.data)
+                query = f"select input_live_sore_ball_type('{data}'::jsonb);"
+                result = execute_query(query)
+                return Response(result, status=status.HTTP_200_OK)
+
+        @api_view(['GET','POST'])
+        def get_batsman_aggrigation_data(request, matchid):
+            if request.method =='GET':
+                query = f"select get_batsman_aggrigation_data('{matchid}');"
+                result = execute_query(query)
+                return Response(result, status=status.HTTP_200_OK)
+
+            elif request.method == 'POST':
+                return Response('post data')
+
+        @api_view(['GET','POST'])
+        def get_bowler_aggrigation_data(request, matchid):
+            if request.method =='GET':
+                query = f"select get_bowler_aggrigation_data('{matchid}');"
+                result = execute_query(query)
+                return Response(result, status=status.HTTP_200_OK)
+
+            elif request.method == 'POST':
+                return Response('post data')
+
+        @api_view(['GET','POST'])
+        def get_fielding_aggrigation_data(request, matchid):
+            if request.method =='GET':
+                query = f"select get_fielding_aggrigation_data('{matchid}');"
+                result = execute_query(query)
+                return Response(result, status=status.HTTP_200_OK)
+
+            elif request.method == 'POST':
+                return Response('post data')
+
+        @api_view(['GET','POST'])
+        def get_start_match_game(request, matchid):
+            if request.method =='GET':
+                query = f"select get_match_start('{matchid}');"
+                result = execute_query(query)
+                return Response(result, status=status.HTTP_200_OK)
+
+            elif request.method == 'POST':
+                return Response('post data')
