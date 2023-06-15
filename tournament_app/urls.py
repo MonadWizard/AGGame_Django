01674543cc2164ab6991416c86c4b1984199d091
MonadWizard <monad.wizard.r@gmail.com>
2023-06-15 12:28:16 +0600

    15_june_2023

--- tournament_app/urls.py
+++ tournament_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("TournamentCreation/",TournamentCreation,name="TournamentCreation",),
     path("get_tournament_details/",get_tournament_details,name="get_tournament_details",),
 
-    path("get_insert_team_data/<str:tournamentid>",get_insert_team_data,name="get_insert_team_data",),
+    path("get_insert_team_data/<str:tournamentid>/",get_insert_team_data,name="get_insert_team_data",),
 
     path('get_tournament_info/<str:user_id>/<int:limit>/<int:offset>/', get_tournament_info, name='get_tournament_info'),
     path('add_tournament_schedule/', tournament_schedule, name='tournament_schedule'),
